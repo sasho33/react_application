099683d681e85e8515ae981977c9fb6daf6f024d
sasho33 <sasho3333333@gmail.com>
2022-12-28 16:02:58 +0000

    modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { useState } from 'react';
 import './index.scss';
 
+const Modal = ({ open, children, toggleModal }) => {
+  return (
+    <div className={`overlay animated ${open ? 'show' : ''}`}>
+      <div className="modal">
+        <svg onClick={toggleModal} height="200" viewBox="0 0 200 200" width="200">
+          <title />
+          <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+        </svg>
+        {children}
+      </div>
+    </div>
+  );
+};
+
 function App() {
   const [open, setOpen] = useState(false);
 
@@ -13,20 +27,9 @@ function App() {
       <button onClick={toggleModal} className="open-modal-btn">
         ✨ Открыть окно
       </button>
-      {open && (
-        <div className="overlay">
-          <div className="modal">
-            <svg height="200" viewBox="0 0 200 200" width="200">
-              <title />
-              <path
-                onClick={toggleModal}
-                d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"
-              />
-            </svg>
-            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-          </div>
-        </div>
-      )}
+      <Modal open={open} setOpen={setOpen} toggleModal={toggleModal}>
+        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+      </Modal>
     </div>
   );
 }
